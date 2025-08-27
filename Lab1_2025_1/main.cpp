e44ec8740f8b8d538aa10f1d4dd387421b362ad1
Razu <a20231846@pucp.edu.pe>
2025-08-26 22:29:46 -0500

    Lab N1 ahora si completo

--- Lab1_2025_1/main.cpp
+++ Lab1_2025_1/main.cpp
@@ -10,10 +10,9 @@ using namespace std;
 #define B 2  // Número de columnas
 #define C 4  // Número de ya
 
-bool verify(int recurso[]) ;
-int sumaDependientes(int dependecia[],int costoSeguridad[A][B]) ;
 bool verifySales(int cromosoma2[A],double montoMax,double montoMin,double &sumaTotal,int costoSeguridad[A][B]) ;
-bool verify(int cromosoma2[A],int dependencia[A][C],double &sumaTotal,int costoSeguridad[A][B]);
+bool verifyDependence(int cromosoma2[A],int dependencia[A][C],int costoSeguridad[A][B]);
+bool verifySecurity(int cromosoma2[A],double seguridadMin,double &sumaTotal,int costoSeguridad[A][B]) ;
 
 // Convierte numero en base M y lo guarda en arreglo asignacion
 void paseBase(int numero, int base, int cromosoma[N], int size) {
@@ -103,56 +102,57 @@ int main() {
         {1, 2, 5, 0}       // Gestión Centralizada -> Firewall, IDS, IPS
     };
     int combinaciones = pow(2,A);
-    double montoMax = 80;
+    double montoMax = 200;
+    double seguridadMin = 400;
     double montoMin = montoMax * 0.8;
     double sumaTotalPrecio;
-    double sumaTotalDependicia;
+    double sumaTotalSeguridad;
     int cromosoma2[A];
+    bool encontro = false;
     for (int i = 0; i < combinaciones; i++) {
         paseBase(i, 2, cromosoma2, A);
         if (!verifySales(cromosoma2,montoMax,montoMin,sumaTotalPrecio,costoSeguridad)) continue;
-        if (!verify(cromosoma2,dependencia,sumaTotalDependicia,costoSeguridad))continue;
-        cout<<i<<"  ";
-        for (int t = 0; t < N; t++) {
-            cout<<cromosoma2[t]<<" ";
-        }
-        cout<<endl;
+        if (!verifySecurity(cromosoma2,seguridadMin,sumaTotalSeguridad,costoSeguridad)) continue;
+        if (!verifyDependence(cromosoma2,dependencia,costoSeguridad))continue;
+        encontro = true;
+        cout<<"Combinacion "<<i<<" Recursos: ";
+        for (int t = 0; t < A; t++)
+            if (cromosoma2[t] == 1) cout << "R"<<(t+1)<<" ";
+        cout<<"| Costo: "<<sumaTotalPrecio<<" miles | Nivel de Segurirda: "<< sumaTotalSeguridad<<endl;
     }
+    if (encontro == false) cout<<"No se encontraron combinaciones que cumplan con lo solicitado."<<endl;
     return 0;
 }
+// verifica si el monto sumado esta dentro del selecionado, aqui todavia no se ve las dependencias
 bool verifySales(int cromosoma2[A],double montoMax,double montoMin,double &sumaTotal,int costoSeguridad[A][B]) {
     sumaTotal = 0;
-    for (int i = 0; i < A; i++) {
-        if (cromosoma2[i] == 1) {
-            sumaTotal += costoSeguridad[i][0];
-        }
-    }
+    //recorre el cromosoma y verifica si esta prendido, luego suma el costo de la posicion
+    for (int i = 0; i < A; i++)
+        if (cromosoma2[i] == 1) sumaTotal += costoSeguridad[i][0];
+    //verifica si esta dentro del rango
     if (sumaTotal >= montoMin && sumaTotal <= montoMax) return true;
     return false;
 }
-
-bool verify(int cromosoma2[A],int dependencia[A][C],double &sumaTotal,int costoSeguridad[A][B]){
+// Lo mismo que el verifySales pero ahora sumas Seguridad
+bool verifySecurity(int cromosoma2[A],double seguridadMin,double &sumaTotal,int costoSeguridad[A][B]) {
     sumaTotal = 0;
-    for (int i = 0; i < A; i++) {
-        if (cromosoma2[i] == 1) {
+    for (int i = 0; i < A; i++)
+        if (cromosoma2[i] == 1) sumaTotal += costoSeguridad[i][1];
+    if (sumaTotal >= seguridadMin) return true;
+    return false;
+}
+
+bool verifyDependence(int cromosoma2[A],int dependencia[A][C],int costoSeguridad[A][B]){
+    //recorre el cromosoma
+    for (int i = 0; i < A; i++)
+        if (cromosoma2[i] == 1)
+            //recorre ahora el arrego de dependecia por fila, si hay 0 ent rompe
+            //si es 1 ent verifica si en el cromosoma con la posicion de la dependecia -1 esta activa
+            // si esta activa ent continua sino returna false
             for (int t= 0; t < C; t++) {
                 if (dependencia[i][t] == 0) break;
                 if (cromosoma2[dependencia[i][t]-1] == 1) continue;
                 else return false;
             }
-            sumaTotal += costoSeguridad[i][1];
-        }
-    }
     return true;
-}
-
-int sumaDependientes(int dependecia[],int costoSeguridad[A][B]) {
-    int sumaParcial= 0;
-    for (int i = 1; i < C; i++) {
-        if (dependecia[i] != 0) {
-            int num = dependecia[i];
-            sumaParcial += costoSeguridad[num-1][0];
-        }
-    }
-    return sumaParcial;
 }
